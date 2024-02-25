3a183f5f5c65fb5542f7352e24beb5df53e03bbf
nay-su <nay@southwestern.edu>
2024-02-25 01:56:45 -0600

    changed tabbar to bottomnavigationbar with state Please enter the commit message for your changes. Lines starting

--- lib/components/nav_bar.dart
+++ lib/components/nav_bar.dart
@@ -1,19 +1,78 @@
 import "package:flutter/material.dart";
 import 'package:flutter_application_1/const.dart';
+import 'package:flutter_application_1/pages/home_page.dart';
+import 'package:flutter_application_1/pages/about_page.dart';
+import 'package:flutter_application_1/pages/account_page.dart';
+import 'package:flutter_application_1/pages/admin_page.dart';
+import 'package:flutter_application_1/pages/feeder_page.dart';
 
-class TopNavBar extends StatelessWidget {
+class TopNavBar extends StatefulWidget {
   const TopNavBar({super.key});
 
   @override
+  State<TopNavBar> createState() => _TopNavBarState();
+}
+
+class _TopNavBarState extends State<TopNavBar> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+
+  static List<Widget> _widgetOptions = <Widget>[
+    HomeScreen(),
+    AboutScreen(),
+    AdminScreen(),
+    FeederScreen(),
+    AccountScreen(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return TabBar(
-      tabs: [
-        Tab(icon: Icon(Icons.home), text: 'Home'),
-        Tab(icon: Icon(Icons.favorite), text: 'About'),
-        Tab(icon: Icon(Icons.person), text: 'Admin'),
-        Tab(icon: Icon(Icons.person), text: 'Feeder Sign Up'),
-        Tab(icon: Icon(Icons.person), text: 'Account'),
-      ],
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: SUYellow,
+        title: const Text('Southwestern Cat Partners'),
+        bottom: PreferredSize(
+          preferredSize: Size.fromHeight(kToolbarHeight),
+          child: BottomNavigationBar(
+            backgroundColor: SUYellow,
+            items: const <BottomNavigationBarItem>[
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                label: 'Home',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.business),
+                label: 'About',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.school),
+                label: 'Admin',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                label: 'Sign Up to Feed',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                label: 'Account',
+              ),
+            ],
+            currentIndex: _selectedIndex,
+            selectedItemColor: Color.fromARGB(255, 255, 255, 255),
+            onTap: _onItemTapped,
+          ),
+        ),
+      ),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
     );
   }
 }
