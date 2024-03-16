d732bc221f5ac5996882eb3591c6dcedba9f090c
annazwicker <amzwickie21@gmail.com>
2024-03-16 18:11:13 -0500

    #15 Added padding to the form
    The form looked to cramped to the edges of the screen so I added padding to make it a bit more readable.

--- lib/pages/account_page.dart
+++ lib/pages/account_page.dart
@@ -44,50 +44,55 @@ class AccountInfoFormState extends State<AccountInfoForm> {
 
   @override
   Widget build(BuildContext context) {
-    return Form(
-      key: _formKey,
-      child: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            _buildTextField('Name', 'Enter your name', (value) {
-              _name = value;
-            }),
-            _buildTextField('Email', 'Enter your email', (value) {
-              _email = value;
-            }),
-            _buildTextField('Phone Number', 'Enter your phone number', (value) {
-              _phoneNumber = value;
-            }),
-            // TODO: should not be a buildTextField, needs to be a dropdown
-            _buildTextField('Status', 'Select your status', (value) {
-              _status = value;
-            }),
-            _buildTextField('Rescue Group Affiliation', 'Enter your rescue group affiliation', (value) {
-              _rescuegroupaffiliation = value;
-            }),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 16),
-              child: ElevatedButton(
-                onPressed: () {
-                  if (_formKey.currentState!.validate()) {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(content: Text('Processing Data')),
-                    );
-        
-                    // Process the collected data (you can send it to a server or save it in a database)
-                    print('Name: $_name, Email: $_email, Phone Number: $_phoneNumber, Status: $_status, Rescue Group Affiliation: $_rescuegroupaffiliation');
-                  }
-                },
-                child: const Text('Submit'),
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Form(
+        key: _formKey,
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              _buildTextField('Name', 'Enter your name', (value) {
+                _name = value;
+              }),
+              _buildTextField('Email', 'Enter your email', (value) {
+                _email = value;
+              }),
+              _buildTextField('Phone Number', 'Enter your phone number', (value) {
+                _phoneNumber = value;
+              }),
+              // TODO: should not be a buildTextField, needs to be a dropdown
+              _buildTextField('Status', 'Select your status', (value) {
+                _status = value;
+              }),
+              _buildTextField('Rescue Group Affiliation', 'Enter your rescue group affiliation', (value) {
+                _rescuegroupaffiliation = value;
+              }),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 16),
+                child: ElevatedButton(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(content: Text('Processing Data')),
+                      );
+          
+                      // Process the collected data (you can send it to a server or save it in a database)
+                      print('Name: $_name, Email: $_email, Phone Number: $_phoneNumber, Status: $_status, Rescue Group Affiliation: $_rescuegroupaffiliation');
+                    }
+                  },
+                  child: const Text('Submit'),
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
   }
 
+// This is the code that creates the name of the text field and the actual box
+// for users to submit information.
   Widget _buildTextField(String title, String hintText, Function(String?) onChanged) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
