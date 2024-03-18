46ca65330a783036e5e841c04d285306de3c0996
nay-su <nay@southwestern.edu>
2024-03-17 23:08:23 -0500

    replaced parent widget with Column. Gave all placeholder widgets padding and margins.

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -43,37 +43,42 @@ class _HomeScreenState extends State<HomeScreen> {
 
   Widget build(BuildContext context) {
     return Center(
-      child: Stack(
-        alignment: AlignmentDirectional.center,
+      child: Column(
+        // alignment: AlignmentDirectional.center,
         children: [
           if (_isWindowOpen)
-            Positioned(
-                top: 25,
-                child: Container(
+            Container(
+              margin: EdgeInsets.all(20),
+              color: SUYellow,
+              height: MediaQuery.of(context).size.height * 0.08,
+              width: MediaQuery.of(context).size.width * 0.85,
+              child: Stack(alignment: AlignmentDirectional.center, children: [
+                Positioned(
+                  right: 0,
+                  top: 0,
+                  child: IconButton(
+                      onPressed: () {
+                        setState(() {
+                          _isWindowOpen = false;
+                        });
+                      },
+                      icon: Icon(Icons.close)),
+                ),
+                Positioned(child: Text('Notifications'))
+              ]),
+            ),
+          Align(
+              alignment: Alignment.centerLeft,
+              child: Container(
                   color: SUYellow,
-                  height: MediaQuery.of(context).size.height * 0.08,
-                  width: MediaQuery.of(context).size.width * 0.85,
-                  child:
-                      Stack(alignment: AlignmentDirectional.center, children: [
-                    Positioned(
-                      right: 0,
-                      top: 0,
-                      child: IconButton(
-                          onPressed: () {
-                            setState(() {
-                              _isWindowOpen = false;
-                            });
-                          },
-                          icon: Icon(Icons.close)),
-                    ),
-                    Positioned(child: Text('Notifications'))
-                  ]),
-                )),
-          Positioned(top: 0, left: 25, child: Text('Achievement Box')),
-          Positioned(
-            top: 150,
-            child: Text('Scheduler'),
-          ),
+                  margin: EdgeInsets.all(20),
+                  padding: EdgeInsets.all(20),
+                  child: Text('Achievement Box'))),
+          Container(
+              color: SUYellow,
+              margin: EdgeInsets.all(20),
+              padding: EdgeInsets.all(20),
+              child: Text('Scheduler')),
         ],
       ),
     );
