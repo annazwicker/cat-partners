1d5e5fd2b6c8fb8ba05919fe92609319d2ebc4cb
MMSU-1987 <matam@southwestern.edu>
2024-03-26 19:34:56 -0500

    put ignore on url_launcher

--- lib/pages/about_page.dart
+++ lib/pages/about_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/gestures.dart';
 import "package:flutter/material.dart";
 import 'package:flutter_application_1/const.dart'; 
+// ignore: depend_on_referenced_packages
 import 'package:url_launcher/url_launcher.dart';
 
 
