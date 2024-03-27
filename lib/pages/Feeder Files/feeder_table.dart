a8ca34fbd902fde3a535a1fb4934a7423663ce1f
JaydenBea <beaucheaj@southwestern.edu>
2024-03-27 10:38:35 -0500

    #29 Added box formatting and dynamic data
    Added formatting via Decorators and Containers to feeder_page. FeederTable can now use a list of maps to pull data from.

--- lib/pages/Feeder Files/feeder_table.dart
+++ lib/pages/Feeder Files/feeder_table.dart
@@ -10,29 +10,40 @@ class FeederTable extends StatefulWidget {
 class _FeederTableState extends State<FeederTable> {
   @override
   Widget build(BuildContext context) {
+    final List<Map<String, dynamic>> data = [
+      {
+        'col1': 'words', 
+        'col2': 'more words'},
+      {
+        'col1': 'yet more words',
+        'col2': 'the bogo bingus' },
+    ];
     return Table(
       border: TableBorder.all(),
-      children: const <TableRow>[
-         TableRow(
-          children: <Widget>[
-            TableCell(
-              verticalAlignment: TableCellVerticalAlignment.middle,
-              child: Text('Words')
-            ),
-            TableCell(
-              verticalAlignment: TableCellVerticalAlignment.middle,
-              child: Text('More words')
-            )
-          ]
-        ),
-        TableRow(
-          children: <Widget>[
-            TableCell(
-              verticalAlignment: TableCellVerticalAlignment.middle,
-              child: Text('yet more words')
-            )
-          ])
-      ],
+      children: [
+        for (var row in data) buildRow(row)
+      ]
+    );
+  }
+
+  TableRow buildRow(Map<String, dynamic> rowData){
+    // TODO: Anticipate date, and a list of actual entries
+    // Build however many columns to hold date info, and a variable
+    // Number of entries, one for each station. Cells should be interactable
+    // to allow user editing.
+    List<TableCell> cells = [];
+    rowData.forEach((key, value) {
+      cells.add(buildCell(Text(value)));
+    });
+    return TableRow(
+      children: cells
+    );
+  }
+
+  TableCell buildCell(dynamic cellData){
+    return TableCell(
+      verticalAlignment: TableCellVerticalAlignment.middle,
+      child: cellData
     );
   }
 }
--- lib/pages/feeder_page.dart
+++ lib/pages/feeder_page.dart
@@ -1,11 +1,41 @@
 import "package:flutter/material.dart";
+import "package:flutter/rendering.dart";
+import "package:flutter/widgets.dart";
+import "package:flutter_application_1/pages/Feeder%20Files/feeder_table.dart";
 
 class FeederScreen extends StatelessWidget {
   const FeederScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text('feeder page'),);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Schedule'),
+      ),
+      body: Row(
+        children: [
+          Flexible ( 
+            child: Container(
+              margin: const EdgeInsets.all(20),
+              constraints: const BoxConstraints(
+                // maxWidth: 600, 
+                minWidth: 600
+              ),
+              // child: const Text('table goes here!!!')
+              child: const FeederTable()
+          )
+          ), 
+          Container(
+            decoration: BoxDecoration(
+              border: Border.all(),
+              borderRadius: const BorderRadius.all(Radius.circular(10))),
+            margin: const EdgeInsets.all(20),
+            constraints: const BoxConstraints(
+              maxWidth: 800,
+              minWidth: 400,
+            ),
+            child: Text('side bar goes here!!!11'))
+        ],)
+    );
   }
 }
