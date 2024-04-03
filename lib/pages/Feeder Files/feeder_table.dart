6461ea0d8379901fb4a241746a8e8ff63c25d246
JaydenBea <beaucheaj@southwestern.edu>
2024-04-02 19:19:24 -0500

    Added month mapping
    Int:String mapping for month abbreviations. Anticipated use with DateTime class.

--- lib/pages/Feeder Files/feeder_table.dart
+++ lib/pages/Feeder Files/feeder_table.dart
@@ -1,5 +1,20 @@
 import 'package:flutter/material.dart';
 
+final Map<int, String> monthMap = {
+  1: 'Jan',
+  2: 'Feb',
+  3: 'Mar',
+  4: 'Apr',
+  5: 'May',
+  6: 'Jun',
+  7: 'Jul',
+  8: 'Aug',
+  9: 'Sep',
+  10: 'Oct',
+  11: 'Nov',
+  12: 'Dec',
+};
+
 class FeederTable extends StatefulWidget {
   const FeederTable({super.key});
 
