1f4678976e8773f44ece799ba77f4fe357ac3977
nay-su <nay@southwestern.edu>
2024-04-05 08:08:09 -0500

    co-opted admin page to attempt to connect fo google funtions. Currently unsuccessful

--- lib/pages/admin_page.dart
+++ lib/pages/admin_page.dart
@@ -5,24 +5,28 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 Future<Album> createAlbum(String title) async {
-  final response = await http.post(
-    Uri.parse('https://jsonplaceholder.typicode.com/albums'),
-    headers: <String, String>{
-      'Content-Type': 'application/json; charset=UTF-8',
-    },
-    body: jsonEncode(<String, String>{
-      'title': title,
-    }),
-  );
+  try {
+    final response = await http.post(
+      Uri.parse(
+          'https://us-central1-catpartners.cloudfunctions.net/get-catpartner'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String, String>{
+        'title': title,
+      }),
+    );
 
-  if (response.statusCode == 201) {
-    // If the server did return a 201 CREATED response,
-    // then parse the JSON.
-    return Album.fromJson(jsonDecode(response.body) as Map<String, dynamic>);
-  } else {
-    // If the server did not return a 201 CREATED response,
-    // then throw an exception.
-    throw Exception('Failed to create album.');
+    if (response.statusCode == 200) {
+      return Album.fromJson(jsonDecode(response.body) as Map<String, dynamic>);
+    } else {
+      throw Exception(
+          'Failed to create album. Server responded with status code ${response.statusCode}');
+    }
+  } catch (e) {
+    // Handle exception
+    print('Error creating album: $e');
+    throw Exception('Failed to create album: $e');
   }
 }
 
