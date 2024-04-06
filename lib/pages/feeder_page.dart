35c3b28e940fd09472c266597c990b19ab5d46cc
JaydenBea <beaucheaj@southwestern.edu>
2024-04-05 19:52:35 -0500

    Convert to Stateful + Encapsulate
    Made FeederPage Stateful and moved table+sidebar wrapper widgets to methods.

--- lib/pages/feeder_page.dart
+++ lib/pages/feeder_page.dart
@@ -3,40 +3,53 @@ import "package:flutter/material.dart";
 import "Feeder Files/feeder_sidebar.dart";
 import "Feeder Files/feeder_table.dart";
 
-class FeederScreen extends StatelessWidget {
+class FeederScreen extends StatefulWidget {
   const FeederScreen({super.key});
 
   @override
+  State<FeederScreen> createState() => _FeederScreenState();
+}
+
+class _FeederScreenState extends State<FeederScreen> {
+  
+  Widget tableWrap(FeederTable table) {
+    return Flexible ( 
+      child: Container(
+        margin: const EdgeInsets.all(20),
+        constraints: const BoxConstraints(
+          // maxWidth: 600, 
+          minWidth: 600
+        ),
+        child: table
+      )
+    ); 
+  }
+
+  Widget sidebarWrap(FeederSidebar sidebar) {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(),
+        borderRadius: const BorderRadius.all(Radius.circular(10))),
+      margin: const EdgeInsets.all(20),
+      constraints: const BoxConstraints(
+        maxWidth: 400,
+        minWidth: 400,
+      ),
+      child: sidebar
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
+    FeederTable fTable = const FeederTable();
+    FeederSidebar fSidebar = const FeederSidebar();
+    
     return Scaffold(
       appBar: AppBar(
         title: const Text('Schedule'),
       ),
       body: Row(
-        children: [
-          Flexible ( 
-            child: Container(
-              margin: const EdgeInsets.all(20),
-              constraints: const BoxConstraints(
-                // maxWidth: 600, 
-                minWidth: 600
-              ),
-              // child: const Text('table goes here!!!')
-              child: const FeederTable()
-            )
-          ), 
-          Container(
-            decoration: BoxDecoration(
-              border: Border.all(),
-              borderRadius: const BorderRadius.all(Radius.circular(10))),
-            margin: const EdgeInsets.all(20),
-            constraints: const BoxConstraints(
-              maxWidth: 400,
-              minWidth: 400,
-            ),
-            child: const FeederSidebar()
-          )
-        ],
+        children: [tableWrap(fTable), sidebarWrap(fSidebar)],
       )
     );
   }
