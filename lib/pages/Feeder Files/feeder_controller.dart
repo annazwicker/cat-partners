dbbb4c18597f86ee04fa1fe79aa9cb10e5bb648a
JaydenBea <beaucheaj@southwestern.edu>
2024-04-07 12:48:29 -0500

    Added Controller
    FeederController to hold common data on Feeder page.

---
+++ lib/pages/Feeder Files/feeder_controller.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+enum PageState { 
+  empty,  // Default view. Displays basic information.
+  select, // Entry selection. User selects multiple entries.
+  view,   // View an entry, the user's or another's.
+  }
+
+class FeederController extends ChangeNotifier {
+  PageState currentState = PageState.empty;
+
+  // Shared
+  late int? currentUserID; // placeholder; should be global variable across app
+
+  // Entry select
+  late List<Map<String, dynamic>>? selectedEntries;
+
+  // Entry view
+  late Map<String, dynamic>? currentEntry;
+  
+}
