3b4b74617e68b80f92775e1826f5add4d6398ddc
nay-su <nay@southwestern.edu>
2024-04-07 18:48:26 -0500

    attempted to establish firestore db connection
    created cloud firestore cat-db in test mode
    added cloud firestore dependency
    created firebase firestore instance in firebase_helper
    grabbed/created document with primary key of "stationID" from collection 'station'
    hard coded a station document instance for testing purposes for set method
    temporarily converting feeder page to run saveStation method
    
    turned feedingStation into a stateful widget
    added button that calls saveStation method on click
    connection doesn't seem to be establishing. There is no change in the database

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,17 +7,39 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-import * as functions from "firebase-functions";
-import * as admin from "firebase-admin";
+// import * as functions from "firebase-functions";
+// import * as admin from "firebase-admin";
+// admin.initializeApp();
+// const fcm = admin.messaging();
+
+// exports.checkHealth = functions.https.onCall(async (data, context) => {
+//     return "The function is online";
+// });
+
+// exports.sendNotification = functions.https.onCall(async (data, context) => {
+//     const title = data.title;
+//     const body = data.body;
+//     const image = data.image;
+//     const token = data.token;
 
 
-exports.checkHealth = functions.https.onCall(async (data, context) => {
-    return "The function is online";
-});
+//     try{
+//         const payload = {
+//             token: token,
+//             notification: {
+//                 title: title,
+//                 body: body,
+//                 image: image,
+//             },
+//             data: {
+//                 body: body,
+//             },
 
-exports.sendNotification = functions.https.onCall(async (data, context) => {
-    const title = data.title;
-    const body = data.body;
-    const image = data.image;
-    const token = data.token;
-});
+//         };
+//         return fcm.send(payload).then((response) => {
+//             return {success: true, response: "Successfully sent message: " + response}
+//         }).catch()(error) => {
+//             return {error: error};
+//         };
+//     }
+// });
--- lib/services/firebase_helper.dart
+++ lib/services/firebase_helper.dart
@@ -1,13 +1,65 @@
 
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class FirebaseHelper {
   const FirebaseHelper._();
 
   static final FirebaseAuth _auth = FirebaseAuth.instance;
-  // static final FirebaseFirestore _db = FirebaseFirestore.instance;
+
+  static final FirebaseFirestore _db = FirebaseFirestore.instance;
+
+
+
+  static Future <bool> saveStation({
+    // required BuildContext context,
+    required int stationID,
+    required String name,
+    required String description,
+    required String photo,
+  }) async{
+    try{
+      //either grab or create a document from a collection
+
+        // Create a new user with a first and last name
+        final user = <String, dynamic>{
+          "first": "Ada",
+          "last": "Lovelace",
+          "born": 1815
+        };
+
+        // Add a new document with a generated ID
+        print(user);
+        print(_db);
+        _db.collection("users").add(user).then((DocumentReference doc) =>
+            print('DocumentSnapshot added with ID: ${doc.id}'));
+
+
+
+      var stationRef = _db.collection('station').doc("0");
+
+      //create hard coded json document example
+      Map <String, dynamic> hardCodedJson = {
+        'stationID' : "0",
+        'name' : 'Admissions',
+        'description' : 'Admissions back patio',
+        'photo' : 'placeholder.img',
+      };
+
+      //set document to a specific value
+      stationRef.set(hardCodedJson);
+
+      print("Entered the try block at least");
+      return true;
+      
+    } catch(e){
+      return false;
+    }
+  }
+  
 
   static Future <bool> saveUser({
     required BuildContext context,
