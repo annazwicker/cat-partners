e3b081d4e8db3b97e6dee159d66f517b4434ae45
JaydenBea <beaucheaj@southwestern.edu>
2024-04-07 17:48:56 -0500

    #29 Moved test data to own file
    Moved test data from FeederTable to its own file. Also added 'date' param to CellWrapper.

--- lib/pages/Feeder Files/feeder_table.dart
+++ lib/pages/Feeder Files/feeder_table.dart
@@ -1,22 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/pages/Feeder%20Files/feeder_controller.dart';
-import 'package:intl/intl.dart'; // DateFormat
+import 'package:intl/intl.dart';
 
-// To be used after date cell reformatting
-final Map<int, String> monthMap = {
-  1: 'Jan',
-  2: 'Feb',
-  3: 'Mar',
-  4: 'Apr',
-  5: 'May',
-  6: 'Jun',
-  7: 'Jul',
-  8: 'Aug',
-  9: 'Sep',
-  10: 'Oct',
-  11: 'Nov',
-  12: 'Dec',
-};
+import 'feeder_test_data.dart'; // Test data for dev
 
 class FeederTable extends StatefulWidget {
   const FeederTable({super.key,
@@ -27,109 +13,6 @@ class FeederTable extends StatefulWidget {
   @override
   State<FeederTable> createState() => _FeederTableState();
 }
-
-// Stations + cats
-final List<int> testStationIDs = [0, 1, 2];
-final Map<int, Map<String, dynamic>> testStationData = {
-  0: {
-    'name': 'Caskey Lords',
-    'cats': ['Patches', 'Super Cal', 'Ziggy']
-  },
-  1: {
-    'name': 'Mabee',
-    'cats': ['Pumpkin', 'Princess']
-  },
-  2: {
-    'name': 'Admissions',
-    'cats': ['Gray Mama', 'Itty Bitty', 'Gaia', 'Teddy']
-  },
-};
-
-// TODO REPLACE with call to DB
-final List<String> listOfStations = ['Caskey', 'Mabee', 'Admin'];
-
-// Placeholder variables
-const String feedIDString = 'feederID';
-const String noteIDString = 'notes';
-const String sightingsIDString = 'sightings';
-final List<Map<String, dynamic>> testData = [
-      {
-        'date': DateTime.utc(2024, 1, 1), 
-        'data': [
-          {feedIDString: 'Joe Schmoe',
-           noteIDString: 'Saw Super Cal yelling at me',
-           sightingsIDString: [true, true, false],
-          },
-          
-          {feedIDString: 'Joe Schmoe',
-           noteIDString: '',
-           sightingsIDString: [true, true],
-          },
-          
-          {feedIDString: 'Argus',
-           noteIDString: '',
-           sightingsIDString: [false, false, false, false],
-          },
-        ]
-      },
-      {
-        'date': DateTime.utc(2024, 1, 2), 
-        'data': [
-          {feedIDString: 'Mary Sue',
-           noteIDString: 'No one around :(',
-           sightingsIDString: [false, false, false],
-          },
-          
-          {feedIDString: 'Just Joe',
-           noteIDString: '',
-           sightingsIDString: [true, true],
-          },
-          
-          {feedIDString: 'Tori Vanderbucks',
-           noteIDString: 'Saw a silly little guy',
-           sightingsIDString: [false, true, false, false],
-          },
-        ]
-      },
-      {
-        'date': DateTime.utc(2024, 1, 3), 
-        'data': [
-          {feedIDString: 'Smorgus Borde',
-           noteIDString: 'woah!!!1!!',
-           sightingsIDString: [true, false, false],
-          },
-          
-          {feedIDString: 'Smorgus Borde',
-           noteIDString: '',
-           sightingsIDString: [true, true],
-          },
-          
-          {feedIDString: 'Smorgus Borde',
-           noteIDString: '',
-           sightingsIDString: [false, true, false, true],
-          },
-        ]
-      },
-      {
-        'date': DateTime.utc(2024, 1, 4), 
-        'data': [
-          {feedIDString: null,
-           noteIDString: '',
-           sightingsIDString: [false, false, false],
-          },
-          
-          {feedIDString: 'Felid Day',
-           noteIDString: '',
-           sightingsIDString: [true, true],
-          },
-          
-          {feedIDString: 'Felid Day',
-           noteIDString: '',
-           sightingsIDString: [false, false, false, false],
-          },
-        ]
-      },
-    ];
 
 class _FeederTableState extends State<FeederTable> {
   @override
@@ -171,6 +54,7 @@ class _FeederTableState extends State<FeederTable> {
       // CellWrapper holds all cell data
       cells.add(buildCell(CellWrapper(
         data: entry, 
+        date: rowData['date'],
         controller: widget.controller
       )));
     }
@@ -205,9 +89,11 @@ class _FeederTableState extends State<FeederTable> {
 class CellWrapper extends StatelessWidget{
   const CellWrapper({super.key,
   required this.data,
+  required this.date,
   required this.controller});
 
   // TODO ought to be an entryID to a shared data from DB call
+  final DateTime date;
   final Map<String, dynamic> data;
   final FeederController controller;
 
