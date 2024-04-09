ebfbc38e9200ae253e080fc28b47533c7e6cb9bb
nay-su <nay@southwestern.edu>
2024-04-09 10:48:57 -0500

    got firestore updates to work

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,39 +7,13 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-// import * as functions from "firebase-functions";
-// import * as admin from "firebase-admin";
-// admin.initializeApp();
-// const fcm = admin.messaging();
+import {onRequest} from "firebase-functions/v2/https";
+import * as logger from "firebase-functions/logger";
 
-// exports.checkHealth = functions.https.onCall(async (data, context) => {
-//     return "The function is online";
-// });
+// Start writing functions
+// https://firebase.google.com/docs/functions/typescript
 
-// exports.sendNotification = functions.https.onCall(async (data, context) => {
-//     const title = data.title;
-//     const body = data.body;
-//     const image = data.image;
-//     const token = data.token;
-
-
-//     try{
-//         const payload = {
-//             token: token,
-//             notification: {
-//                 title: title,
-//                 body: body,
-//                 image: image,
-//             },
-//             data: {
-//                 body: body,
-//             },
-
-//         };
-//         return fcm.send(payload).then((response) => {
-//             return {success: true, response: "Successfully sent message: " + response}
-//         }).catch()(error) => {
-//             return {error: error};
-//         };
-//     }
+// export const helloWorld = onRequest((request, response) => {
+//   logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
 // });
