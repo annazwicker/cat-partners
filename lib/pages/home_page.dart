adfaba987e75bf0dd6de75a4016846e702a41820
nay-su <nay@southwestern.edu>
2024-04-12 19:40:24 -0500

    created new test page for database test statements

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,9 +1,5 @@
 import "package:flutter/material.dart";
 import "package:flutter_application_1/components/nav_bar.dart";
-import 'package:flutter_application_1/pages/about_page.dart';
-import 'package:flutter_application_1/pages/account_page.dart';
-import 'package:flutter_application_1/pages/admin_page.dart';
-import 'package:flutter_application_1/pages/feeder_page.dart';
 import 'package:flutter_application_1/const.dart';
 
 
@@ -15,15 +11,6 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  final int _currentIndex = 0;
-
-  final List<Widget> _pages = [
-    const HomeScreen(),
-    AboutScreen(),
-    const AdminScreen(),
-    const FeederScreen(),
-    const AccountScreen(),
-  ];
 
   @override
   Widget build(BuildContext context) {
