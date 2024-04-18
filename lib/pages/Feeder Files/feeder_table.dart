abb5d7f85d2da5b0b0029c56e4e430e65c49f2e2
JaydenBea <beaucheaj@southwestern.edu>
2024-04-18 18:32:52 -0500

    #29 Altered code to call DB

--- lib/pages/Feeder Files/feeder_table.dart
+++ lib/pages/Feeder Files/feeder_table.dart
@@ -1,9 +1,13 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/pages/Feeder%20Files/feeder_controller.dart';
 import 'package:intl/intl.dart';
 
+import '../../models/entry.dart';
+import '../../models/station.dart';
+import '../../models/userdoc.dart';
 import '../../services/firebase_helper.dart';
-import 'feeder_test_data.dart'; // Test data for dev
+import 'package:collection/collection.dart';
 
 class FeederTable extends StatefulWidget {
   const FeederTable({super.key,
@@ -18,50 +22,126 @@ class FeederTable extends StatefulWidget {
 }
 
 class _FeederTableState extends State<FeederTable> {
+
+  // late Stream<QuerySnapshot<Entry>> entryStream;
+  late List<QueryDocumentSnapshot<Station>> stations;
+
+  Future<List<QueryDocumentSnapshot<Station>>> _initStations() async {
+    // bool isError = false;
+    List<QueryDocumentSnapshot<Station>> listStations = [];
+    await widget.fh.stationsRef.get().then(
+      (stationSnapshot) {
+        listStations = stationSnapshot.docs;
+      },
+      onError: (e) {
+        print(e);
+        // isError = true;
+      }
+    );
+    listStations.sort((a, b) {
+      return a.id.compareTo(b.id);
+    });
+    return listStations;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Table(
-      border: TableBorder.all(),
-      children: [
-        headerRow(),
-        for (var row in testData) buildRow(row)
-      ]
+    return FutureBuilder(
+      future: _initStations(),
+      builder: (stationContext, stationSnapshot) {
+        if (!stationSnapshot.hasData) {
+          return const CircularProgressIndicator();
+        }
+        stations = stationSnapshot.data!;
+        return StreamBuilder(
+          stream: widget.fh.getEntryStream(),
+          builder: (context, snapshot) {
+            List<QueryDocumentSnapshot<Entry>> allEntries = snapshot.data?.docs ?? [];
+        
+            // Groups entries by date
+            Map<Timestamp, List<QueryDocumentSnapshot<Entry>>> groupedEntries = groupBy(allEntries, 
+              (p0) {
+                return widget.fh.equalizeTime(p0.data().date);
+              }
+            );
+        
+            // Debug: print all entries
+            void debugPrint () {
+              groupedEntries.forEach((key, value) {
+              print(key.toDate());
+              for (QueryDocumentSnapshot<Entry> ent in value) {
+                print("\t ${ent.data()}");
+              }
+              },);
+            }
+        
+            List<TableRow> rows = buildAllRows(groupedEntries);
+            
+            return Table(
+              border: TableBorder.all(),
+              children: [headerRow()] + buildAllRows(groupedEntries),          
+            );
+          }
+        );
+      }
     );
   }
 
-  /// Builds the table's header row with a list of stations.
-  TableRow headerRow(){
+  List<TableRow> buildAllRows(Map<Timestamp, List<QueryDocumentSnapshot<Entry>>> groupedEntries) {
+    assert (stations != null);
+    List<TableRow> rows = [];
+    groupedEntries.forEach((stamp, snapshots) { 
+      DateTime date = stamp.toDate();
+      // Ensure data for this row is valid (iterate through stations)
+      bool rowIsValid = true;
+      if(snapshots.length == stations!.length) {
+        // There must exist an entry for each station
+        for (QueryDocumentSnapshot<Station> station in stations!) {
+          // Find entries with this stationID
+          if(!snapshots.any((element) => element.data().getStationID().id == station.id)){
+            rowIsValid = false;
+            break;
+          }
+        }
+      } else { rowIsValid = false; }
+
+      if(rowIsValid) {
+        rows.add(buildRow(stamp, snapshots));
+      } 
+      else { // debug
+        print('Row invalid: $date');
+      }
+
+    });
+    // TODO sort rows...
+    return rows; // TODO
+  }
+  
+  TableRow buildRow(Timestamp date, List<QueryDocumentSnapshot<Entry>> data){
     List<TableCell> cells = [];
-    cells.add(buildCell(const Text('Date')));
-    for (var station in listOfStations) {
-      cells.add(buildCell(Text(station)));
+    // Add date first
+    var format = DateFormat('yyyy-MM-dd');
+    String formattedDate = format.format(date.toDate());
+    cells.add(buildCell(Text(formattedDate)));
+    for (int i = 0; i < data.length; i++) {
+      // CellWrapper holds all cell data
+      cells.add(buildCell(CellWrapper(
+        data: data[i], 
+        controller: widget.controller
+      )));
     }
     return TableRow(
       children: cells
     );
   }
 
-  /// Builds a row with the given Map.
-  TableRow buildRow(Map<String, dynamic> rowData){
-    // TODO: Anticipate date, and a list of actual entries
-    // Build however many columns to hold date info, and a variable
-    // Number of entries, one for each station. Cells should be interactable
-    // to allow user editing.
-
+  /// Builds the table's header row with a list of stations.
+  TableRow headerRow(){
+    assert (stations != null);
     List<TableCell> cells = [];
-    // Add date first
-    var format = DateFormat('yyyy-MM-dd');
-    String formattedDate = format.format(rowData['date']);
-    cells.add(buildCell(Text(formattedDate)));
-    // for (var entry in rowData['data']) {
-    for (int i = 0; i < rowData['data'].length; i++) {
-      // CellWrapper holds all cell data
-      cells.add(buildCell(CellWrapper(
-        data: rowData['data'][i], 
-        date: rowData['date'],
-        station: testStationData[i]!,
-        controller: widget.controller
-      )));
+    cells.add(buildCell(const Text('Date')));
+    for (var station in stations!) {
+      cells.add(buildCell(Text(station.data().name)));
     }
     return TableRow(
       children: cells
@@ -81,36 +161,42 @@ class _FeederTableState extends State<FeederTable> {
 class CellWrapper extends StatelessWidget{
   const CellWrapper({super.key,
   required this.data,
-  required this.date,
-  required this.station,
+  // required this.date,
+  // required this.station,
   required this.controller});
 
   // TODO ought to be an entryID to a shared data from DB call
-  final DateTime date;
-  final Map<String, dynamic> station;
-  final Map<String, dynamic> data;
+  // final DateTime date;
+  // final Map<String, dynamic> station;
+  final QueryDocumentSnapshot<Entry> data;
   final FeederController controller;
 
   @override
   Widget build(BuildContext context) {
-    String feederName = optToString(data[feedIDString]);
-    data['date'] = date;
-    data['station'] = station['name'];
-    data['cats'] = station['cats'];
+    DocumentReference? userID = data.data().assignedUser;
+    String feederName = '';
+    if(userID != null){
+      userID.get().then((value) {
+        // print("not null!");
+        UserDoc? userData = UserDoc.fromJson(value.data() as Map<String, dynamic>) ;
+        feederName = "${userData.first} ${userData.last}";
+      });
+    }
 
     return Container(
       padding: const EdgeInsets.all(8.0),
       child: GestureDetector(
         onTap: () {
+          // TODO re-enable
           // Enter selection for empty entries
-          if (data[feedIDString] == null){
-            controller.toSelectState();
-            controller.toggleSelection(data);
-          }
-          // Enter view for assigned entries
-          else {
-            controller.toViewState(data);
-          }          
+          // if (data[feedIDString] == null){
+          //   controller.toSelectState();
+          //   controller.toggleSelection(data);
+          // }
+          // // Enter view for assigned entries
+          // else {
+          //   controller.toViewState(data);
+          // }          
         },
         child: Text(feederName),
       ),
