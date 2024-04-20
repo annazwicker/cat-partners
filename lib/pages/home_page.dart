9341774e7197fe9377cbcbdffdcf719595c6fa13
nay-su <nay@southwestern.edu>
2024-04-20 17:11:51 -0500

    connected achievement widget to backend. It now keeps track of how many entries the user has filled in

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -116,15 +116,6 @@ class _HomeScreenState extends State<HomeScreen> {
         children: [
           NotificationBox(message: notificationMessage,),
 
-          Align(
-              alignment: Alignment.centerLeft,
-              child: Container(
-                  width: MediaQuery.of(context).size.width * 0.3,
-                  height: MediaQuery.of(context).size.width * 0.1,
-                  color: SUYellow,
-                  margin: const EdgeInsets.all(20),
-                  padding: const EdgeInsets.all(20),
-                  child: const Text('Achievement Box'))),
           Container(
               width: MediaQuery.of(context).size.width * 0.75,
               height: MediaQuery.of(context).size.width * 0.3,
@@ -138,62 +129,50 @@ class _HomeScreenState extends State<HomeScreen> {
               }
             ),
             //achievement box
-            StreamBuilder(
-              stream: _dbHelper.getUpcomingUserEntries(_dbHelper.getCurrentUser()),
-              builder: (context, snapshot) {
-                List entries = snapshot.data?.docs ?? [];
-                entries.sort((a, b) {
-                  DateTime aE = a.data().date.toDate();
-                  DateTime bE = b.data().date.toDate();
-                  DateTime aDT = DateTime(aE.year, aE.month, aE.day);
-                  DateTime bDT = DateTime(bE.year, bE.month, bE.day);
-                  int comp = aDT.compareTo(bDT);
-                  if(comp == 0){
-                    String aS = a.data().stationID.id;
-                    String bS = b.data().stationID.id;
-                    return aS.compareTo(bS);
-                  }
-                  return comp;
-                });
-                if(entries.isEmpty) {
-                  return const Center(
-                    child: Text('No entries found.')
-                  );
-                }
-                return Flexible(
-                  child: ListView.builder(
-                    // physics: const AlwaysScrollableScrollPhysics(),
-                    shrinkWrap: true,
-                    itemCount: entries.length,
-                    itemBuilder: (context, index) {
-                      Entry entry = entries[index].data();
-                      // String entryId = entries[index].id;
-                      return Padding(
-                        padding: const EdgeInsets.symmetric(
-                          vertical: 10,
-                          horizontal: 10,
-                        ),
-                        child: SingleChildScrollView(
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: [
-                              Flexible(child: Text(entry.date.toDate().toString())),
-                              Flexible(child: Text(entry.assignedUser?.id ?? 'nullUser')),
-                              Flexible(child: Text(entry.note)),
-                              Flexible(child: Text(entry.stationID.id))
-                            ],
-                          ),
-                        )
-                      );
-                    },
-                  ),
-                );
-              }
-            ),
+            test_widget(dbHelper: _dbHelper),
             
           ]
         )
       )
+    );
+  }
+}
+
+class test_widget extends StatelessWidget {
+  const test_widget({
+    super.key,
+    required FirebaseHelper dbHelper,
+  }) : _dbHelper = dbHelper;
+
+  final FirebaseHelper _dbHelper;
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: _dbHelper.getUpcomingUserEntries(_dbHelper.getCurrentUser()),
+      builder: (context, snapshot) {
+        List entries = snapshot.data?.docs ?? [];
+        
+        String achievementText = "";
+        if(entries.isEmpty) {
+          achievementText = "No cats fed yet! Sign up for an entry and add 1 to the count!";
+        }
+        else{
+          achievementText = "You have filled in " + entries.length.toString() + " entries!\n Thank you so much!";
+        }
+        return Flexible(
+          child:           Align(
+              alignment: Alignment.centerLeft,
+              child: Container(
+                  width: MediaQuery.of(context).size.width * 0.3,
+                  height: MediaQuery.of(context).size.width * 0.1,
+                  color: SUYellow,
+                  margin: const EdgeInsets.all(20),
+                  padding: const EdgeInsets.all(20),
+                  child: Text(achievementText))),
+
+        );
+      }
     );
   }
 }
