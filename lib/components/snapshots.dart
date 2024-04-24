7bcd0ddbd308f9cc74f10e883844886c23ababde
JaydenBea <beaucheaj@southwestern.edu>
2024-04-24 11:18:52 -0500

    #48 Created skeleton for Snapshots class

---
+++ lib/components/snapshots.dart
@@ -0,0 +1,14 @@
+import 'package:flutter_application_1/services/firebase_helper.dart';
+
+/// Contains common data used by various pages of the database.
+/// Facilitates access to such data.
+class Snapshots {
+  FirebaseHelper fh = FirebaseHelper();
+
+  // Snapshots:
+  // Stream of entries (Table)
+  // List of stations
+  // List of users
+  // List of cats
+  
+}
