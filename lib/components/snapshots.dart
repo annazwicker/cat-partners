8c5c69fcdaeaa8d283c950c1982fedede16ddb2e
JaydenBea <beaucheaj@southwestern.edu>
2024-04-25 20:42:23 -0500

    #48 Made Snapshots static + fixed getDocument

--- lib/components/snapshots.dart
+++ lib/components/snapshots.dart
@@ -10,43 +10,43 @@ import '../models/station.dart';
 /// Contains common data used by various pages of the database.
 /// Facilitates access to such data.
 class Snapshots {
-  FirebaseHelper fh = FirebaseHelper();
+  static FirebaseHelper fh = FirebaseHelper();
 
 
   // Late instance variables 
   
-  late final Stream<QuerySnapshot<Entry>> _entryStream = fh.getEntryStream();
-  late final Stream<QuerySnapshot<Cat>> _catStream = fh.getCatStream();
-  late final Stream<QuerySnapshot<Station>> _stationStream = fh.getStationStream();
-  late final Stream<QuerySnapshot<UserDoc>> _userStream = fh.getUserStream();
+  static final Stream<QuerySnapshot<Entry>> _entryStream = fh.getEntryStream();
+  static final Stream<QuerySnapshot<Cat>> _catStream = fh.getCatStream();
+  static final Stream<QuerySnapshot<Station>> _stationStream = fh.getStationStream();
+  static final Stream<QuerySnapshot<UserDoc>> _userStream = fh.getUserStream();
 
   
-  late Future<List<QueryDocumentSnapshot<Station>>> _stationQuery;
-  late Future<List<QueryDocumentSnapshot<Entry>>> _entryQuery;
-  late Future<List<QueryDocumentSnapshot<Cat>>> _catQuery;
-  late Future<List<QueryDocumentSnapshot<UserDoc>>> _userQuery;
+  static Future<List<QueryDocumentSnapshot<Station>>> _stationQuery = _initStations();
+  static Future<List<QueryDocumentSnapshot<Entry>>> _entryQuery = _initEntries();
+  static Future<List<QueryDocumentSnapshot<Cat>>> _catQuery = _initCats();
+  static Future<List<QueryDocumentSnapshot<UserDoc>>> _userQuery = _initUsers();
 
   /// A Stream returning all entries. Dynamically updates.
-  Stream<QuerySnapshot<Entry>> get entryStream => _entryStream;
+  static Stream<QuerySnapshot<Entry>> get entryStream => _entryStream;
   /// A Stream returning all cats. Dynamically updates.
-  Stream<QuerySnapshot<Cat>> get catStream => _catStream;
+  static Stream<QuerySnapshot<Cat>> get catStream => _catStream;
   /// A Stream returning all stations. Dynamically updates.
-  Stream<QuerySnapshot<Station>> get stationStream => _stationStream;
+  static Stream<QuerySnapshot<Station>> get stationStream => _stationStream;
   /// A Stream returning all users. Dynamically updates.
-  Stream<QuerySnapshot<UserDoc>> get userStream => _userStream;
+  static Stream<QuerySnapshot<UserDoc>> get userStream => _userStream;
 
 
   /// Returns a list of stations. Does not dynamically update.
-  Future<List<QueryDocumentSnapshot<Station>>>  get stationQuery => _stationQuery;
+  static Future<List<QueryDocumentSnapshot<Station>>>  get stationQuery => _stationQuery;
   /// Returns a list of entries. Does not dynamically update.
-  Future<List<QueryDocumentSnapshot<Entry>>>    get entryQuery => _entryQuery;
+  static Future<List<QueryDocumentSnapshot<Entry>>>    get entryQuery => _entryQuery;
   /// Returns a list of cats. Does not dynamically update.
-  Future<List<QueryDocumentSnapshot<Cat>>>      get catQuery => _catQuery;
+  static Future<List<QueryDocumentSnapshot<Cat>>>      get catQuery => _catQuery;
   /// Returns a list of users. Does not dynamically update.
-  Future<List<QueryDocumentSnapshot<UserDoc>>>  get userQuery => _userQuery;
+  static Future<List<QueryDocumentSnapshot<UserDoc>>>  get userQuery => _userQuery;
 
   /// Returns all documents in the collection referenced by [ref].
-  Future<List<QueryDocumentSnapshot<T>>> _initQuery<T>(CollectionReference<T> ref) async {
+  static Future<List<QueryDocumentSnapshot<T>>> _initQuery<T>(CollectionReference<T> ref) async {
     // bool isError = false;
     List<QueryDocumentSnapshot<T>> results = [];
     await ref.get().then(
@@ -62,7 +62,7 @@ class Snapshots {
   }
   
   /// Returns DocumentSnapshots for all Stations in the collection.
-  Future<List<QueryDocumentSnapshot<Station>>> _initStations() async {
+  static Future<List<QueryDocumentSnapshot<Station>>> _initStations() async {
     List<QueryDocumentSnapshot<Station>> listStations = [];
     await _initQuery(fh.stationsRef).then((value) => listStations = value);
     listStations.sort((a, b) {
@@ -72,31 +72,24 @@ class Snapshots {
   }
 
   /// Returns DocumentSnapshots for all Users in the collection.
-  Future<List<QueryDocumentSnapshot<UserDoc>>> _initUsers() async {
+  static Future<List<QueryDocumentSnapshot<UserDoc>>> _initUsers() async {
     List<QueryDocumentSnapshot<UserDoc>> listUsers = [];
     await _initQuery(fh.usersRef).then((value) => listUsers = value);
     return listUsers;
   }
 
   /// Returns DocumentSnapshots for all Cats in the collection.
-  Future<List<QueryDocumentSnapshot<Cat>>> _initCats() async {
+  static Future<List<QueryDocumentSnapshot<Cat>>> _initCats() async {
     List<QueryDocumentSnapshot<Cat>> listUsers = [];
     await _initQuery(fh.catsRef).then((value) => listUsers = value);
     return listUsers;
   }
 
   /// Returns DocumentSnapshots for all Entries in the collection.
-  Future<List<QueryDocumentSnapshot<Entry>>> _initEntries() async {
+  static Future<List<QueryDocumentSnapshot<Entry>>> _initEntries() async {
     List<QueryDocumentSnapshot<Entry>> listUsers = [];
     await _initQuery(fh.entriesRef).then((value) => listUsers = value);
     return listUsers;
-  }
-  
-  Snapshots(){
-    _stationQuery = _initStations();
-    _catQuery = _initCats();
-    _entryQuery = _initEntries();
-    _userQuery = _initUsers();
   }
 
   // Methods
@@ -104,16 +97,16 @@ class Snapshots {
   /// Returns a QueryDocumentSnapshot with type [T], given a DocumentReference referring
   /// to a document in the collection modelled by [T]. Throws an exception if T is not
   /// Station, UserDoc, Entry or Cat.
-  Future<QueryDocumentSnapshot<T>> getDocument<T extends Model>(DocumentReference docRef) async {
+  static Future<QueryDocumentSnapshot<T>> getDocument<T extends Model>(DocumentReference docRef) async {
     Future<List<QueryDocumentSnapshot<T>>> query;
-    switch(T.runtimeType){
-      case Station _:
+    switch(T){
+      case Station:
         query = _stationQuery as Future<List<QueryDocumentSnapshot<T>>>;
-      case Cat _:
+      case Cat:
         query = _catQuery as Future<List<QueryDocumentSnapshot<T>>>;
-      case Entry _:
+      case Entry:
         query = _entryQuery as Future<List<QueryDocumentSnapshot<T>>>;
-      case UserDoc _:
+      case UserDoc:
         query = _userQuery as Future<List<QueryDocumentSnapshot<T>>>;
       default:
         throw FormatException('DocumentReference is not in the right collection: ${docRef.parent.runtimeType}.'
@@ -131,7 +124,7 @@ class Snapshots {
   /// entry in [entrySnapshot]. The bool in the first part of the tuple
   /// is true if and only if such a user exists; i.e., if [entrySnapshot] both has
   /// a non-null assignedUser field and that user exists in the database.
-  Future<(bool, DocumentSnapshot<UserDoc>?)> getAssignedUser(QueryDocumentSnapshot<Entry> entrySnapshot) async {
+  static Future<(bool, DocumentSnapshot<UserDoc>?)> getAssignedUser(QueryDocumentSnapshot<Entry> entrySnapshot) async {
     /// A tuple with a bool is used to allow this Future to return a non-null value
     /// even when the given Entry has no assignedUser. Otherwise, FutureBuilders using
     /// this function won't be able to differentiate waiting on this function from the
@@ -150,11 +143,11 @@ class Snapshots {
 
   /// TEMPORARY FUNCTION.
   /// Returns whether there is a user logged in.
-  bool isUserLoggedIn() { return fh.isUserLoggedIn; }
+  static bool isUserLoggedIn() { return fh.isUserLoggedIn; }
 
   /// TEMPORARY FUNCTION.
   /// Returns the DocumentReference of the current HARD-CODED user.
-  Future<DocumentReference> getCurrentUserTEST() async {
+  static Future<DocumentReference> getCurrentUserTEST() async {
     assert (isUserLoggedIn());
     String currentUserID = fh.currentUserIDTest!;
     return await fh.usersRef.doc(currentUserID);
